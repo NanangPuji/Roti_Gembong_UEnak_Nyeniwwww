4ac92034a2e191dcad90edbde69bd78f79db014d
Nanang Puji <nanangpuji762@gmail.com>
2023-11-09 11:17:28 +0700

    Initial commit

---
+++ app/src/main/java/com/example/esjumbo/data/SumberData.kt
@@ -0,0 +1,16 @@
+package com.example.esjumbo.data
+
+import com.example.esjumbo.R
+
+
+object SumberData {
+    val flavors = listOf(
+        R.string.Coklat,
+        R.string.Vanila,
+        R.string.Chococran,
+        R.string.milk,
+        R.string.Tiramisu,
+
+
+    )
+}
